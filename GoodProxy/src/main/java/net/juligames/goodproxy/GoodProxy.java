09d0fc1bbeeda9b0a770de5c48eea781c741c312
Ture Bentzin <ture.bentzin@alumni.fh-aachen.de>
2025-02-08 15:53:20 +0100

    stop useless logs

--- GoodProxy/src/main/java/net/juligames/goodproxy/GoodProxy.java
+++ GoodProxy/src/main/java/net/juligames/goodproxy/GoodProxy.java
@@ -5,16 +5,13 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.jetbrains.annotations.NotNull;
 
-import javax.swing.*;
-import java.awt.image.LookupOp;
-
 /**
  * @author Ture Bentzin
  * @since 08-02-2025
  */
-public class Main {
+public class GoodProxy {
 
-    protected static final @NotNull Logger LOGGER = LogManager.getLogger(Main.class);
+    protected static final @NotNull Logger LOGGER = LogManager.getLogger(GoodProxy.class);
 
     public static void main(@NotNull String @NotNull [] args) {
 
